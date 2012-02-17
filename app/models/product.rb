1bb1d2876b1cb48b2a16ed56704f3634f5141270
Egor Lynko <flexoid@gmail.com>
2012-02-17 19:52:23 +0300

    Playtime after B. Custom error message

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,5 @@ class Product < ActiveRecord::Base
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)$}i,
     message: 'must be a URL for GIF, JPG or PNG image.'}
-  validates :title, length: {minimum: 10 }
+  validates :title, length: {minimum: 10, too_short: 'must be at least 10 characters.' }
 end

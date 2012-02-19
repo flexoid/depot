98ae0474e1f525a0eae7e6e8eecaa5528515efcc
Egor Lynko <flexoid@gmail.com>
2012-02-19 17:02:39 +0300

    Seeds for carts and line_items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,6 @@
-#---
-# Excerpted from "Agile Web Development with Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
-# encoding: utf-8
 Product.delete_all
 Product.create(title: 'CoffeeScript',
-  description: 
+  description:
     %{<p>
         CoffeeScript is JavaScript done right. It provides all of JavaScript's
 	functionality wrapped in a cleaner, more succinct syntax. In the first
@@ -17,9 +8,9 @@ Product.create(title: 'CoffeeScript',
 	shows you how to hold onto all the power and flexibility of JavaScript
 	while writing clearer, cleaner, and safer code.
       </p>},
-  image_url:   'cs.jpg',    
+  image_url:   'cs.jpg',
   price: 36.00)
-# . . .
+
 Product.create(title: 'Programming Ruby 1.9',
   description:
     %{<p>
@@ -29,10 +20,9 @@ Product.create(title: 'Programming Ruby 1.9',
       </p>},
   image_url: 'ruby.jpg',
   price: 49.95)
-# . . .
 
 Product.create(title: 'Rails Test Prescriptions',
-  description: 
+  description:
     %{<p>
         <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
         Rails applications, covering Test-Driven Development from both a
@@ -43,3 +33,17 @@ Product.create(title: 'Rails Test Prescriptions',
       </p>},
   image_url: 'rtp.jpg',
   price: 34.95)
+
+
+Cart.delete_all
+3.times do
+  Cart.create!
+end
+
+
+LineItem.delete_all
+LineItem.create!(product: Product.all[0], cart: Cart.all[0])
+LineItem.create!(product: Product.all[1], cart: Cart.all[1])
+LineItem.create!(product: Product.all[2], cart: Cart.all[2])
+LineItem.create!(product: Product.all[1], cart: Cart.all[0])
+LineItem.create!(product: Product.all[2], cart: Cart.all[1])

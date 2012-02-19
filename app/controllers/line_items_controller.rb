6b6147d119151847cb812cb0043423887cd7a381
Egor Lynko <flexoid@gmail.com>
2012-02-19 17:33:20 +0300

    Playtime after D. Counter

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -50,6 +50,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
+        session[:counter] = 0
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else

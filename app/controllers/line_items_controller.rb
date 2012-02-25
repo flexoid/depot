cf7e80c881af53256b019f1e97f2b39a02753a5b
Egor Lynko <flexoid@gmail.com>
2012-02-25 21:52:06 +0300

    Task F. 'Add to Cart' with ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -61,6 +61,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         session[:counter] = 0
         format.html { redirect_to store_url }
+        format.js
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }

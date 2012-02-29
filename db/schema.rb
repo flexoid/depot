0f7c6c507ed5e6c176187b7ab19b47c220687924
Egor Lynko <flexoid@gmail.com>
2012-02-29 12:48:32 +0300

    Name field for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120228162602) do
+ActiveRecord::Schema.define(:version => 20120229093749) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20120228162602) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
